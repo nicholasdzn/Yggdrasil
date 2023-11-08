303973eca073c84f057ad14a40fed8d3e97d6144
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-07 23:47:10 -0300

    added chat and chatbar

--- src/components/ChatBar/ChatBar.jsx
+++ src/components/ChatBar/ChatBar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import "./style.css"
 const ChatBar = () => {
     return (
-        <p>ola</p>
+        <nav className='flex h-full flex-col nav-chat justify-around bg-slate-500'>
+            <div>
+                <ol>
+                    <li>
+                        <a href="">Chat1</a>
+                    </li>
+                    <li>
+                        <a href="">Chat2</a>
+                    </li>
+                </ol>
+            </div>
+            <div>
+                <h2>Usuário</h2>
+            </div>
+        </nav>
     )
 }
 
