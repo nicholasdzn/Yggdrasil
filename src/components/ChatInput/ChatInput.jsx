27237dba39883f1e5221bdbde94533470ad71363
Gustavo Berti <gu_cju@hotmail.com>
2023-11-14 19:51:30 -0300

    Estilizando ChatInput

--- src/components/ChatInput/ChatInput.jsx
+++ src/components/ChatInput/ChatInput.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { IoMdSend } from "react-icons/io";
 
 const ChatInput = ({ value, handleInput, handleSubmit }) => {
     return (
-        <div className='flex flex-row justify-center'>
-            <input type="text" className='rounded-l outline-none px-3 py-2 bg-transparent text-white border border-white w-full' value={value} onChange={handleInput} />
-            <button className='p-3 bg-green-400 rounded-r' onClick={handleSubmit}><IoMdSend fill='#fff' /></button>
+        <div className='flex flex-row justify-center bg-transparent outline outline-white outline-[2px] rounded-full'>
+          <textarea 
+            type="text"
+            placeholder="Faça sua pergunta..." 
+            className="rounded-full bg-transparent flex-1 px-4 pt-3 text-white focus:outline-none resize-none" 
+            value={value} 
+            onChange={handleInput}
+            style={{ height: '50px' }} 
+          />
+          <button 
+            className="bg-green-400 text-white rounded-full font-semibold px-8 hover:bg-green-300 focus:outline-none" 
+            onClick={handleSubmit}
+          >
+            <IoMdSend size={'20px'} />
+          </button>       
         </div>
-    )
+    );
 }
 
-export default ChatInput
+export default ChatInput;
