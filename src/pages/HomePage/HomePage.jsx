56a408e0cb23c7f6bd6106eaca76712e7a9b8232
Gustavo Berti <gu_cju@hotmail.com>
2023-11-16 00:08:39 -0300

    Mudanças no HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Logo from "../../Assets/Img/LogoChat2.png"
+import Yggdrasil from "../../Assets/Img/IggDrasil.png"
+import "./Style.css"
 const HomePage = () => {
     return (
-        <Header />
-    )
+        <div className='bg-white flex flex-col h-screen'>
+            <Header />
+            <div className='flex flex-1 items-center justify-center'>
+                <div className='flex flex-col items-center text-center'>
+                    <h1 className='text-black text-4xl mb-4'>Bem-vindo ao Nosso Site!</h1>
+                    <p className='text-black mb-6'>Explore as maravilhas do nosso conteúdo.</p>
+                    <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
+                </div>
+                <img src={Logo} alt='Descrição da Imagem' className='max-w-md' />
+            </div>
+        </div>
+    );
 }
 
-export default HomePage
