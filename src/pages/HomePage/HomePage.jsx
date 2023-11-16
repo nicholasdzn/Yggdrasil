aa3a15e9c93db72a96f8c73aff39acf093ff0397
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-16 19:05:07 -0300

    feito o design da home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Logo from "../../Assets/Img/LogoChat2.png"
-import Yggdrasil from "../../Assets/Img/IggDrasil.png"
 import "./Style.css"
 const HomePage = () => {
     return (
-        <div className='bg-white flex flex-col h-screen'>
+        <>
             <Header />
-            <div className='flex flex-1 items-center justify-center'>
-                <div className='flex flex-col items-center text-center'>
-                    <h1 className='text-black text-4xl mb-4'>Bem-vindo ao Nosso Site!</h1>
-                    <p className='text-black mb-6'>Explore as maravilhas do nosso conteúdo.</p>
+            <section className='flex flex-col sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content'>
+                <div className='flex flex-col items-center justify-center text-center order-last sm:order-1'>
+                    <h1 className='text-4xl mb-4 font-bold'>Bem-vindo ao Nosso Site!</h1>
+                    <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
                     <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
                 </div>
-                <img src={Logo} alt='Descrição da Imagem' className='max-w-md' />
-            </div>
-        </div>
+                <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
+            </section>
+        </>
     );
 }
 
+export default HomePage;
