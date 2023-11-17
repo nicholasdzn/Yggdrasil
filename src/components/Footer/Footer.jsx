71a26afba4494552d927df669139cdb903b83c80
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-16 21:40:33 -0300

    fixed home page background

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
         <footer className="bg-slate-900 text-white flex flex-col items-center text-center p-4">
             <div className='pb-2'><Logo height={50} width={150} /></div>
             <p>©{new Date().getFullYear()} Desenvolvido por TMNG DEVS. Todos os direitos reservados.</p>
-            <div>
-                <a href="/termos" className="text-blue-300 hover:text-blue-500">Termos de Uso</a> | 
+            <div className='flex flex-row gap-2'>
+                <a href="/termos" className="text-blue-300 hover:text-blue-500">Termos de Uso</a> |
                 <a href="/privacidade" className="text-blue-300 hover:text-blue-500">Política de Privacidade</a>
             </div>
         </footer>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,16 +6,18 @@ import "./Style.css"
 const HomePage = () => {
     return (
         <>
-            <Header />
-            <section className='flex flex-col h-screen sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content'>
-                <div className='flex flex-col items-center justify-center text-center order-last sm:order-1'>
-                    <h1 className='text-4xl mb-4 font-bold'>Bem-vindo ao Nosso Site!</h1>
-                    <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
-                    <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
-                </div>
-                <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
-            </section>
-            <Footer />
+            <div className='flex flex-col h-screen'>
+                <Header />
+                <section className='flex flex-col sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content flex-1'>
+                    <div className='flex flex-col items-center justify-center text-center order-last sm:order-1'>
+                        <h1 className='text-4xl mb-4 font-bold'>Bem-vindo ao Nosso Site!</h1>
+                        <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
+                        <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
+                    </div>
+                    <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
+                </section>
+                <Footer />
+            </div>
         </>
     );
 }
