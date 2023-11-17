ef4165645c2356416b2ef3b54893cfba08069e56
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-17 00:06:34 -0300

    fixed <p> size

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,17 +11,11 @@ const HomePage = () => {
                 <Header />
                 <section className='flex flex-col sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content flex-1'>
                     <div className='flex flex-col items-start justify-center order-last sm:order-1'>
-                        <div className='flex flex-col text-[4rem] mb-10 text-left font-bold text-[#22577A]'>
-                            <h1 className='leading-tight'>Welcome to our</h1> {/* Ajuste no leading e mb */}
-                            <h1 className='leading-tight'>Website!</h1>
-                        </div>
-                        
-                        <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
-                        
-                
-                        <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
+                        <h1 className='text-4xl mb-4 font-bold 2xl:text-6xl text-[#22577A]'>Welcome to our Website</h1>
+                        <p className='mb-6 2xl:text-4xl max-w-[25rem] 2xl:max-w-[50rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat corporis blanditiis dolor sapiente odit dolorem sit! Perferendis molestias, saepe quasi pariatur amet in. Recusandae facilis autem a vitae doloribus.</p>
+                        <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 2xl:w-[20rem] 2xl:h-[5rem] 2xl:text-4xl 2xl:p-3'>Descubra Mais</button>
                     </div>
-                    <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
+                    <img src={Logo} alt="" className='w-[15rem] 2xl:w-[20rem] order-1' />
                 </section>
                 <Footer />
             </div>
