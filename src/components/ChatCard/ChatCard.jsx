e9b7b30559b84287701ac597fff1b885bae6bff0
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-17 16:36:40 -0300

    fixed chatbar

--- src/components/ChatCard/ChatCard.jsx
+++ src/components/ChatCard/ChatCard.jsx
@@ -3,13 +3,13 @@ import { BsChatLeft } from 'react-icons/bs'
 
 const ChatCard = ({ content }) => {
     return (
-        <div>
+        <>
             {content.map((item, index) =>
                 <div className='mt-2 flex flex-row items-center gap-3 p-2 text-white hover:bg-zinc-700 cursor-pointer' key={index}>
                     <BsChatLeft fill='#fff' />
                     <p>{item.ChatName}</p>
                 </div>)}
-        </div>
+        </>
     )
 }
 
