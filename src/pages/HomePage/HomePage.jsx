349ff7ce5d6be872fdd3be086c1dc0b4eeb959d5
Gustavo Berti <gu_cju@hotmail.com>
2023-11-16 22:55:39 -0300

    Atualizações na tela HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Logo from "../../Assets/Img/LogoChat2.png"
-import "./Style.css"
+import "./Style.css";
+
 const HomePage = () => {
     return (
         <>
@@ -10,8 +11,14 @@ const HomePage = () => {
                 <Header />
                 <section className='flex flex-col sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content flex-1'>
                     <div className='flex flex-col items-center justify-center text-center order-last sm:order-1'>
-                        <h1 className='text-4xl mb-4 font-bold'>Bem-vindo ao Nosso Site!</h1>
-                        <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
+                        <div className='flex flex-col text-[4rem] mb-10 text-left font-bold text-[#22577A]'>
+                            <h1 className='leading-tight mb-0'>Welcome to our</h1> {/* Ajuste no leading e mb */}
+                            <h1 className='leading-tight'>Website!</h1>
+                        </div>
+                        <div className='pr-[11rem] text-left'>
+                            <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
+                        </div>
+                        
                         <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Descubra Mais</button>
                     </div>
                     <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
