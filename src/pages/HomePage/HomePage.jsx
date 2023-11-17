34fe285522ae2ff19000d2d45c3918dc9436ab51
Gustavo Berti <gu_cju@hotmail.com>
2023-11-16 21:11:21 -0300

    Alterações na HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import Logo from "../../Assets/Img/LogoChat2.png"
 import "./Style.css"
 const HomePage = () => {
     return (
         <>
             <Header />
-            <section className='flex flex-col sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content'>
+            <section className='flex flex-col h-screen sm:flex-row items-center justify-center pt-5 gap-5 pb-5 main-content'>
                 <div className='flex flex-col items-center justify-center text-center order-last sm:order-1'>
                     <h1 className='text-4xl mb-4 font-bold'>Bem-vindo ao Nosso Site!</h1>
                     <p className='mb-6'>Explore as maravilhas do nosso conteúdo.</p>
@@ -14,6 +15,7 @@ const HomePage = () => {
                 </div>
                 <img src={Logo} alt="" className='w-[15rem] sm:w-[20rem] order-1' />
             </section>
+            <Footer />
         </>
     );
 }
