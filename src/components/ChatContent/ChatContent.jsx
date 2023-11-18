b5484ac98c437d279577b10ad4b29cc43e679395
Thiago M. Martins <thiagomantovanimartins@outlook.com>
2023-11-18 13:39:53 -0300

    Added writing animation

--- src/components/ChatContent/ChatContent.jsx
+++ src/components/ChatContent/ChatContent.jsx
@@ -3,6 +3,7 @@ import { ImSphere } from "react-icons/im";
 import ChatInput from '../ChatInput/ChatInput';
 import "./style.css"
 import { TypeAnimation } from 'react-type-animation';
+import { useRef, useEffect, useState } from 'react';
 
 // const ChatContent = ({ children }) => {
 //     return (
@@ -22,8 +23,52 @@ import { TypeAnimation } from 'react-type-animation';
 //     )
 // }
 
+const ChatContent = ({ children, content }) => {
+  console.log('Conteudo recebido', content);
+
+  const lastRenderedMessageIndexRef = useRef(-1);
+  const [firstRender, setFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (content.length > 0) {
+      lastRenderedMessageIndexRef.current = content.length - 1;
+      if (firstRender) {
+        setFirstRender(false);
+      }
+    }
+  }, [content, firstRender]);
+
+  return (
+    <>
+      <div className='bg-zinc-700 gridChat w-screen h-screen'>
+        <div className='overflow-scroll px-20 text-white flex flex-col gap-3 py-5'>
+          {content.map((message, index) => {
+            const key = Object.keys(message)[0];
+            const value = message[key];
+            const isNewMessage = index > lastRenderedMessageIndexRef.current && !firstRender;
+            return (
+              <div className='flex flex-row items-center gap-3 pb-2 border-solid' key={index}>
+                <p>{key === 'user' ? 'model' : 'user'}</p>
+                <div className={`p-2 ${key === 'user' ? 'bg-green-600' : 'bg-blue-600'} rounded`}>
+                  {key === 'model' && isNewMessage ? (
+                    <TypeAnimation speed={100} sequence={[value]}></TypeAnimation>
+                  ) : (
+                    <span>{value}</span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className='bg-zinc-600 flex flex-col justify-center px-20'>{children}</div>
+      </div>
+    </>
+  );
+};
+
+
 // const ChatContent = ({ children, content }) => {
-//   console.log('Conteudo recebido', content);
+//   console.log("Conteudo recebido", content);
 //   return (
 //     <>
 //       <div className='bg-zinc-700 gridChat w-screen h-screen'>
@@ -35,48 +80,18 @@ import { TypeAnimation } from 'react-type-animation';
 //               <div className='flex flex-row items-center gap-3 pb-2 border-solid' key={index}>
 //                 <p>{key === 'user' ? 'model' : 'user'}</p>
 //                 <div className={`p-2 ${key === 'user' ? 'bg-green-600' : 'bg-blue-600'} rounded`}>
-//                   {key === 'model' ? (
-//                     <TypeAnimation speed={50} sequence={[value]}></TypeAnimation>
-//                   ) : (
-//                     <span>{value}</span>
-//                   )}
+//                   {value}
 //                 </div>
 //               </div>
 //             );
 //           })}
 //         </div>
-//         <div className='bg-zinc-600 flex flex-col justify-center px-20'>{children}</div>
+//         <div className='bg-zinc-600 flex flex-col justify-center px-20'>
+//           {children}
+//         </div>
 //       </div>
 //     </>
-//   );
-// };
-
-
-const ChatContent = ({ children, content }) => {
-  console.log("Conteudo recebido", content);
-  return (
-    <>
-      <div className='bg-zinc-700 gridChat w-screen h-screen'>
-        <div className='overflow-scroll px-20 text-white flex flex-col gap-3 py-5'>
-          {content.map((message, index) => {
-            const key = Object.keys(message)[0];
-            const value = message[key];
-            return (
-              <div className='flex flex-row items-center gap-3 pb-2 border-solid' key={index}>
-                <p>{key === 'user' ? 'model' : 'user'}</p>
-                <div className={`p-2 ${key === 'user' ? 'bg-green-600' : 'bg-blue-600'} rounded`}>
-                  {value}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className='bg-zinc-600 flex flex-col justify-center px-20'>
-          {children}
-        </div>
-      </div>
-    </>
-  )
-}
+//   )
+// }
 
 export default ChatContent
