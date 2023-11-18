23f147f9ae039440a2cc2405655f823510376ca1
Nicholas Adorni <nicholasadorni1643@gmail.com>
2023-11-18 14:30:30 -0300

    componentized ChatMsg and style improved

---
+++ src/components/ChatMsg/ChatMsg.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+import { FaUser } from "react-icons/fa";
+import { AiOutlineGlobal } from "react-icons/ai";
+
+const ChatMsg = ({ content, lastRenderedMessageIndexRef, firstRender }) => {
+    return (
+        <>
+            {
+                content.map((message, index) => {
+                    const key = Object.keys(message)[0];
+                    const value = message[key];
+                    const isNewMessage = index > lastRenderedMessageIndexRef.current && !firstRender;
+                    return (
+                        <div className='flex flex-row items-center gap-3 pb-2 border-solid' key={index}>
+                            <div className={`p-2 rounded ${key === 'user' ? 'bg-green-600' : 'bg-violet-800'}`}>{key === 'user' ? <FaUser /> : <AiOutlineGlobal />}</div>
+                            <div className={`p-2`}>
+                                {key === 'model' && isNewMessage ? (
+                                    <TypeAnimation speed={100} sequence={[value]}></TypeAnimation>
+                                ) : (
+                                    <span>{value}</span>
+                                )}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
+
+    )
+}
+
+export default ChatMsg
